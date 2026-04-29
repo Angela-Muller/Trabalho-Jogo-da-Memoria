5dc219abfd40e79a3e48fd971c25aaa3aba86f23
Angela-Muller <Am.angelamuller@gmail.com>
2026-04-29 14:05:06 -0300

    fix: button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // seleciona todas as imagens e coloca dentro de um 'array'
 const images = document.querySelectorAll(".card-img")
 // seleciona todos os botões e coloca dentro de um 'array'
-const btnCards = document.querySelectorAll(".card-btn")
+const btnCards = document.querySelectorAll(".cards1")
 // seleciona todos os fundos das cartas e coloca dentro de um `array`
 const fundo = document.querySelectorAll(".back")
 // Seleciona todo o container das cartas
@@ -9,19 +9,24 @@ const container = document.querySelector(".cards");
 // Seleciona todos os botões das cartas
 const btnReset = document.getElementById("reset");
 
+const btnstart = document.getElementById("start");
+
+const pont = document.getElementById("pont")
+
+const tpm = document.getElementById("tpm")
+
+let pontuacao = 0;
+
+let tentativa = 0;
+
+let tempo = 0;
+
 // para cara do botão, ele vai adicionar um evento 
 btnCards.forEach((btn, index) => {
   btn.addEventListener('click', () => {
      // pega a imagem (por índice) e ativa
     images[index].classList.toggle('hide')
     fundo[index].classList.toggle('hide')
-
-    // se o texto é 'esconder', troca para 'mostrar'
-    if(btn.textContent === 'ESCONDER') {
-        btn.textContent = 'MOSTRAR'
-    } else {
-        btn.textContent = 'ESCONDER'
-    }
   })  
 })
    
@@ -39,7 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function resetarJogo() {
     const cartas = Array.from(container.children);
-
+    pontuacao = 0;
+    tempo = 0;
+    tpm.innerHTML = `tempo ${tempo}`
+    pont.innerHTML = `pontuacao ${pontuacao}`
     embaralhar(cartas);
 
     container.innerHTML = "";
@@ -53,5 +61,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  function pontuacao_game(){
+    tempo ++;
+    pontuacao +=1
+    tpm.innerHTML = `tempo ${tempo}`
+    pont.innerHTML = `pontuacao ${pontuacao}`
+  }
+
   btnReset.addEventListener("click", resetarJogo);
+  btnstart.addEventListener("click",pontuacao_game)
 });
